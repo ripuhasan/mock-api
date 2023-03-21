0f6184e6f39bce7f1e40383882eff1e311995507
ripuhasan <mahedul.hassan.tri@gmail.com>
2023-03-21 17:17:54 +0600

    mock api permission add in seeder file

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -29,6 +29,12 @@ class PermissionSeeder extends Seeder
             ['name' => 'user-view', 'group' => 'User', 'title' => 'View', 'guard_name' => 'web'],
             ['name' => 'user-edit', 'group' => 'User', 'title' => 'Edit', 'guard_name' => 'web'],
             ['name' => 'user-delete', 'group' => 'User', 'title' => 'Delete', 'guard_name' => 'web'],
+            
+            //Mock Api
+            ['name' => 'mock-api-create', 'group' => 'MockApi', 'title' => 'Create', 'guard_name' => 'web'],
+            ['name' => 'mock-api-view', 'group' => 'MockApi', 'title' => 'View', 'guard_name' => 'web'],
+            ['name' => 'mock-api-edit', 'group' => 'MockApi', 'title' => 'Edit', 'guard_name' => 'web'],
+            ['name' => 'mock-api-delete', 'group' => 'MockApi', 'title' => 'Delete', 'guard_name' => 'web'],
         ];
 
         DB::table('permissions')->insert($permissions);
